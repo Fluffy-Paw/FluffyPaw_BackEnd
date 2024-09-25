68d3d96655714e898508d59b5a5a8a5b4d254bf0
Khang Duy <duyktkse161141@fpt.edu.vn>
2024-09-25 21:20:06 +0700

    add Get Account by role

--- FluffyPaw_Application/ServiceImplements/AccountService.cs
+++ FluffyPaw_Application/ServiceImplements/AccountService.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using FluffyPaw_Application.Services;
+using FluffyPaw_Domain.Entities;
+using FluffyPaw_Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +13,27 @@ namespace FluffyPaw_Application.ServiceImplements
     public class AccountService : IAccountService
     {
         private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public AccountService(IMapper mapper)
+        public AccountService(IMapper mapper, IUnitOfWork unitOfWork)
         {
             _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<PetOwner>> GetPetOwners()
+        {
+            return _unitOfWork.PetOwnerRepository.Get(includeProperties : "Account");
+        }
+
+        public async Task<IEnumerable<StaffAddress>> GetStaffAddresses()
+        {
+            return _unitOfWork.StaffAddressRepository.Get(includeProperties: "Account");
+        }
+
+        public async Task<IEnumerable<StoreManager>> GetStoreManagers()
+        {
+            return _unitOfWork.StoreManagerRepository.Get(includeProperties: "Account");
         }
     }
 }
--- FluffyPaw_Application/Services/IAccountService.cs
+++ FluffyPaw_Application/Services/IAccountService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluffyPaw_Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace FluffyPaw_Application.Services
 {
     public interface IAccountService
     {
-        
+        Task<IEnumerable<PetOwner>> GetPetOwners();
+        Task<IEnumerable<StoreManager>> GetStoreManagers();
+        Task<IEnumerable<StaffAddress>> GetStaffAddresses();
     }
 }
