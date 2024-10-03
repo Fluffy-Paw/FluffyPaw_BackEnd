4a3cd9f00d2d00887e18d5b795b47e39ff4fb5f5
Khang Duy <duyktkse161141@fpt.edu.vn>
2024-10-03 11:44:24 +0700

    Fix Response for FE

--- FluffyPaw_Application/DTO/Response/PetOwnerResponse/PetOwnerResponse.cs
+++ FluffyPaw_Application/DTO/Response/PetOwnerResponse/PetOwnerResponse.cs
@@ -10,6 +10,8 @@ namespace FluffyPaw_Application.DTO.Response.PetOwnerResponse
 {
     public class PetOwnerResponse : IMapFrom<PetOwner>, IMapFrom<Account>
     {
+        public string Username {  get; set; }
+
         public string FullName { get; set; }
 
         public string Gender { get; set; }
