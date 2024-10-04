dbc894e4a48006fbeedbe76e9645a329ec0698b8
Phạm Quốc Đạt <134580007+datpq-dev@users.noreply.github.com>
2024-10-04 13:42:55 +0700

    CRUD Certificate

---
+++ FluffyPaw_Application/DTO/Request/CertificateRequest/UpdateCertificateRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluffyPaw_Application.DTO.Request.CertificateRequest
+{
+    public class UpdateCertificateRequest
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
