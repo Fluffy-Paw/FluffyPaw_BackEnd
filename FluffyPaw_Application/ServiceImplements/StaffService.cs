187fed66c97df83198b4a46fbecffad3d6861d12
Phạm Quốc Đạt <134580007+datpq-dev@users.noreply.github.com>
2024-10-11 05:36:18 +0700

    staff AcceptBooking, DeniedBooking

--- FluffyPaw_Application/ServiceImplements/StaffService.cs
+++ FluffyPaw_Application/ServiceImplements/StaffService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using FluffyPaw_Application.DTO.Request.StoreServiceRequest;
+using FluffyPaw_Application.DTO.Response;
 using FluffyPaw_Application.DTO.Response.FilesResponse;
 using FluffyPaw_Application.DTO.Response.StoreManagerResponse;
 using FluffyPaw_Application.DTO.Response.StoreServiceResponse;
@@ -8,12 +9,14 @@ using FluffyPaw_Domain.CustomException;
 using FluffyPaw_Domain.Entities;
 using FluffyPaw_Domain.Enums;
 using FluffyPaw_Domain.Interfaces;
+using FluffyPaw_Repository.Enum;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace FluffyPaw_Application.ServiceImplements
 {
@@ -88,11 +91,16 @@ namespace FluffyPaw_Application.ServiceImplements
                 throw new CustomException.InvalidDataException("Danh sách yêu cầu lịch trình trống.");
             }
 
-            foreach ( var createScheduleRequest in createStoreServiceRequest.CreateScheduleRequests)
+            foreach (var createScheduleRequest in createStoreServiceRequest.CreateScheduleRequests)
             {
                 if (existingStoreServiceTimes.Contains(createScheduleRequest.StartTime))
                 {
                     throw new CustomException.InvalidDataException($"Thời gian bắt đầu {createScheduleRequest.StartTime} đã tồn tại.");
+                }
+
+                if (createScheduleRequest.StartTime <= System.DateTimeOffset.UtcNow)
+                {
+                    throw new CustomException.InvalidDataException($"Thời gian {createScheduleRequest.StartTime} không phù hợp.");
                 }
 
                 var newStoreService = new StoreService
@@ -113,6 +121,171 @@ namespace FluffyPaw_Application.ServiceImplements
 
             var storeServiceResponses = _mapper.Map<List<StoreServiceResponse>>(storeServices);
             return storeServiceResponses;
+        }
+
+        public async Task<StoreServiceResponse> UpdateStoreService(long id, UpdateStoreServiceRequest updateStoreServiceRequest)
+        {
+            var existingstoreService = _unitOfWork.StoreServiceRepository.GetByID(id);
+            if (existingstoreService == null)
+            {
+                throw new CustomException.DataNotFoundException("Lịch trình không tồn tại.");
+            }
+
+            if (updateStoreServiceRequest.StartTime <= System.DateTimeOffset.Now)
+            {
+                throw new CustomException.InvalidDataException($"Thời gian {updateStoreServiceRequest.StartTime} không phù hợp.");
+            }
+
+            var existingBooking = _unitOfWork.BookingRepository.Get(eb => eb.StoreServiceId == id);
+            if (existingBooking.Any())
+            {
+                throw new CustomException.DataExistException("Khung giờ này đã có người đặt.");
+            }
+
+            _mapper.Map(existingstoreService, updateStoreServiceRequest);
+            _unitOfWork.Save();
+
+            var storeServiceResponses = _mapper.Map<StoreServiceResponse>(existingstoreService);
+            return storeServiceResponses;
+        }
+
+        public async Task<bool> DeleteStoreService(long id)
+        {
+            var existingstoreService = _unitOfWork.StoreServiceRepository.Get(ess => ess.Id == id, includeProperties: "Store").First();
+            if (existingstoreService == null)
+            {
+                throw new CustomException.DataNotFoundException("Lịch trình không tồn tại.");
+            }
+
+            var bookings = _unitOfWork.BookingRepository.Get(b => b.StoreServiceId == existingstoreService.Id
+                                        && b.Status == BookingStatus.Pending.ToString()
+                                        || b.Status == BookingStatus.Accepted.ToString()
+                                        || b.Status == BookingStatus.CheckedIn.ToString()).ToList();
+            if (bookings.Any())
+            {
+                throw new CustomException.DataExistException($"Chi nhánh {existingstoreService.Store.Name} vẫn còn dịch vụ đang được book.");
+            }
+
+            _unitOfWork.StoreServiceRepository.Delete(existingstoreService);
+            _unitOfWork.Save();
+
+            return true;
+        }
+
+        public async Task<List<BookingResponse>> GetAllBookingByStoreServiceId(long id)
+        {
+            var staff = _authentication.GetUserIdFromHttpContext(_contextAccessor.HttpContext);
+            var account = _unitOfWork.AccountRepository.GetByID(staff);
+
+            var store = _unitOfWork.StoreRepository.Get(s => s.AccountId == account.Id && s.Status == true).FirstOrDefault();
+            if (store == null)
+            {
+                throw new CustomException.DataNotFoundException("Cửa hàng đang bị hạn chế hoặc không tồn tại.");
+            }
+
+            var existingstoreService = _unitOfWork.StoreServiceRepository.Get(
+                                                    ss => ss.Id == id 
+                                                    && ss.StoreId == store.Id 
+                                                    && ss.Status == StoreServiceStatus.Available.ToString(), 
+                                                    includeProperties: "Store")
+                                                    .FirstOrDefault();
+            if (existingstoreService == null)
+            {
+                throw new CustomException.DataNotFoundException("Lịch trình không tồn tại hoặc không thuộc quyền quản lý của cửa hàng.");
+            }
+
+            var existingBooking = _unitOfWork.BookingRepository.Get(b => b.StoreServiceId == id).ToList();
+            if (!existingBooking.Any())
+            {
+                throw new CustomException.DataNotFoundException("Không tìm thấy đặt lịch nào cho lịch trình này.");
+            }
+
+            var bookingResponses = _mapper.Map<List<BookingResponse>>(existingBooking);
+            return bookingResponses;
+        }
+
+        public async Task<BookingResponse> GetBookingById(long id)
+        {
+            var existingBooking = _unitOfWork.BookingRepository.GetByID(id);
+            if (existingBooking == null)
+            {
+                throw new CustomException.DataNotFoundException("Không tìm thấy đặt lịch này.");
+            }
+
+            var bookingResponse = _mapper.Map<BookingResponse>(existingBooking);
+            return bookingResponse;
+        }
+
+        public async Task<bool> AcceptBooking(long id)
+        {
+            var staff = _authentication.GetUserIdFromHttpContext(_contextAccessor.HttpContext);
+            var account = _unitOfWork.AccountRepository.GetByID(staff);
+            var store = _unitOfWork.StoreRepository.Get(s => s.AccountId == account.Id && s.Status == true).First();
+            if (store == null)
+            {
+                throw new CustomException.DataNotFoundException("Cửa hàng đang bị hạn chế. Hãy thử lại sau.");
+            }
+
+            var pendingBooking = _unitOfWork.BookingRepository.Get(pb => pb.Id == id
+                                            && pb.Status == BookingStatus.Pending.ToString()).First();
+            if (pendingBooking == null)
+            {
+                throw new CustomException.DataNotFoundException("Không tìm thấy đặt lịch này.");
+            }
+
+            var storeService = _unitOfWork.StoreServiceRepository.Get(ss => ss.Id == pendingBooking.StoreServiceId 
+                                                && ss.StoreId == store.Id 
+                                                && ss.Status == StoreServiceStatus.Available.ToString())
+                                                .FirstOrDefault();
+            if (storeService == null)
+            {
+                throw new CustomException.DataNotFoundException("Lịch trình không tồn tại hoặc không thuộc quyền quản lý của cửa hàng.");
+            }
+
+            pendingBooking.Status = BookingStatus.Accepted.ToString();
+            _unitOfWork.Save();
+
+            //Handle xử lý thanh toán
+
+            //
+
+            return true;
+        }
+
+        public async Task<bool> DeniedBooking(long id)
+        {
+            var staff = _authentication.GetUserIdFromHttpContext(_contextAccessor.HttpContext);
+            var account = _unitOfWork.AccountRepository.GetByID(staff);
+            var store = _unitOfWork.StoreRepository.Get(s => s.AccountId == account.Id && s.Status == true).First();
+            if (store == null)
+            {
+                throw new CustomException.DataNotFoundException("Cửa hàng đang bị hạn chế. Hãy thử lại sau.");
+            }
+
+            var pendingBooking = _unitOfWork.BookingRepository.Get(pb => pb.Id == id
+                                            && pb.Status == BookingStatus.Pending.ToString()).First();
+            if (pendingBooking == null)
+            {
+                throw new CustomException.DataNotFoundException("Không tìm thấy đặt lịch này.");
+            }
+
+            var storeService = _unitOfWork.StoreServiceRepository.Get(ss => ss.Id == pendingBooking.StoreServiceId
+                                                && ss.StoreId == store.Id
+                                                && ss.Status == StoreServiceStatus.Available.ToString())
+                                                .FirstOrDefault();
+            if (storeService == null)
+            {
+                throw new CustomException.DataNotFoundException("Lịch trình không tồn tại hoặc không thuộc quyền quản lý của cửa hàng.");
+            }
+
+            pendingBooking.Status = BookingStatus.Denied.ToString();
+            _unitOfWork.Save();
+
+            //Handle xử lý thanh toán
+
+            //
+
+            return true;
         }
     }
 }
---
+++ FluffyPaw_Domain/Enums/BookingPaymentMethod.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluffyPaw_Domain.Enums
+{
+    public enum BookingPaymentMethod
+    {
+        COD,
+        PayOS
+    }
+}
