985863323a170f1e4f51f756ab22af4e00ebf617
Phạm Quốc Đạt <134580007+datpq-dev@users.noreply.github.com>
2024-11-22 00:41:32 +0700

    Update Program.cs

--- FluffyPaw_API/Program.cs
+++ FluffyPaw_API/Program.cs
@@ -15,7 +15,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("CorsPolicy", builder =>
     {
         builder
-            .WithOrigins("http://localhost:3000", "http://192.168.2.3:3000")
+            .WithOrigins("http://localhost:3000", 
+            "http://192.168.2.3:3000",
+            "http://127.0.0.1:5500")
             //.AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader()
