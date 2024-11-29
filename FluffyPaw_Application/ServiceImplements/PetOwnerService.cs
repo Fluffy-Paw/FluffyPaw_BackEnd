46db2122d42280ef0b2f74160803f9945cc02a76
Khang Duy <duyktkse161141@fpt.edu.vn>
2024-11-29 19:19:44 +0700

    Get top 6 services

--- FluffyPaw_Application/ServiceImplements/PetOwnerService.cs
+++ FluffyPaw_Application/ServiceImplements/PetOwnerService.cs
@@ -1047,5 +1047,33 @@ namespace FluffyPaw_Application.ServiceImplements
 
             return result;
         }
+
+        public async Task<List<StoreServicePOResponse>> Top6StoreServices()
+        {
+            var result = new List<StoreServicePOResponse>();
+
+            var listStoreServices = _unitOfWork.StoreServiceRepository.Get(ss => ss.StartTime > DateTimeOffset.UtcNow && ss.Status == StoreServiceStatus.Available.ToString() && ss.CurrentPetOwner < ss.LimitPetOwner, includeProperties: "Store,Service,Service.ServiceType,Service.Brand").OrderByDescending(ob => ob.Service.TotalRating).Take(6).ToList();
+
+            foreach (var item in listStoreServices)
+            {
+                var service = _mapper.Map<StoreServicePOResponse>(item);
+                service.StoreName = item.Store.Name;
+                service.ServiceName = item.Service.Name;
+                service.ServiceType = item.Service.ServiceType.Name;
+                service.Image = item.Service.Image;
+                service.Cost = item.Service.Cost;
+                service.Duration = item.Service.Duration;
+                service.ServiceTypeId = item.Service.ServiceTypeId;
+                service.BrandName = item.Service.Brand.Name;
+                service.BrandId = item.Service.Brand.Id;
+                service.Description = item.Service.Description;
+                service.BookingCount = item.Service.BookingCount;
+                service.TotalRating = item.Service.TotalRating;
+
+                result.Add(service);
+            }
+
+            return result.Take(100).ToList();
+        }
     }
 }
