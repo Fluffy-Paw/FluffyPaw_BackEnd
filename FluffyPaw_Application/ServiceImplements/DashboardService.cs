aad6bad28fc20a3b844a1f1d94bce60b13d5a3bb
Khang Duy <duyktkse161141@fpt.edu.vn>
2024-11-30 16:10:23 +0700

    Add GetAllStaticsStaff

--- FluffyPaw_Application/ServiceImplements/DashboardService.cs
+++ FluffyPaw_Application/ServiceImplements/DashboardService.cs
@@ -55,7 +55,7 @@ namespace FluffyPaw_Application.ServiceImplements
             var storeServiceServiceIds = storeServices.Select(s => s.ServiceId).ToList();
             var services = _unitOfWork.ServiceRepository.Get(s => storeServiceServiceIds.Contains(s.Id)).ToList();
             var bookings = _unitOfWork.BookingRepository.Get(b => storeServiceIds.Contains(b.StoreServiceId)).ToList();
-            
+
             int numOfReports = 0;
             foreach (var store in stores)
             {
@@ -64,13 +64,13 @@ namespace FluffyPaw_Application.ServiceImplements
             }
 
             List<double> revenues = new List<double>();
-            for (int i = 1; i<=12;  i++)
+            for (int i = 1; i <= 12; i++)
             {
                 var bookingsByMonth = bookings.FindAll(b => b.CreateDate.Month == i);
                 double revenue = bookingsByMonth.Sum(r => r.Cost);
                 revenues.Add(revenue);
             }
-            
+
             List<StoreServiceResponse> topServices = new List<StoreServiceResponse>();
             //var listService = services.OrderByDescending(s => s.BookingCount).Take(3).ToList();
             //foreach (var service in listService)
@@ -110,6 +110,71 @@ namespace FluffyPaw_Application.ServiceImplements
                 NumOfPending = bookings.Count(p => p.Status.Equals(BookingStatus.Pending.ToString())),
                 NumOfStores = stores.Count(),
                 NumOfReports = numOfReports,
+                Revenues = revenues,
+                TopServices = topServices
+            };
+
+            return response;
+        }
+
+        public async Task<SMDashboardResponse> GetAllStaticsStaff()
+        {
+            var userId = _authentication.GetUserIdFromHttpContext(_contextAccessor.HttpContext);
+            var store = _unitOfWork.StoreRepository.Get(b => b.Account.Id.Equals(userId)).FirstOrDefault();
+            var storeServices = _unitOfWork.StoreServiceRepository.Get(s => s.StoreId == store.Id, includeProperties: "Store,Service").ToList();
+            var storeServiceIds = storeServices.Select(s => s.Id).ToList();
+            var storeServiceServiceIds = storeServices.Select(s => s.ServiceId).ToList();
+            var services = _unitOfWork.ServiceRepository.Get(s => storeServiceServiceIds.Contains(s.Id)).ToList();
+            var bookings = _unitOfWork.BookingRepository.Get(b => storeServiceIds.Contains(b.StoreServiceId)).ToList();
+            var report = _unitOfWork.ReportRepository.Get(rp => rp.TargetId.Equals(store.AccountId)).ToList();
+
+            List<double> revenues = new List<double>();
+            for (int i = 1; i <= 12; i++)
+            {
+                var bookingsByMonth = bookings.FindAll(b => b.CreateDate.Month == i);
+                double revenue = bookingsByMonth.Sum(r => r.Cost);
+                revenues.Add(revenue);
+            }
+
+            List<StoreServiceResponse> topServices = new List<StoreServiceResponse>();
+            //var listService = services.OrderByDescending(s => s.BookingCount).Take(3).ToList();
+            //foreach (var service in listService)
+            //{
+            //    var ss = new StoreServiceResponse
+            //    {
+            //        ServiceName = service.Name,
+            //        NumberOfBooking = service.BookingCount
+            //    };
+            //    topServices.Add(ss);
+            //}
+
+            var storeServiceDict = storeServices.ToDictionary(ss => ss.Id);
+            var countTopServices = bookings.GroupBy(b => b.StoreServiceId).Select(g => new { serviceId = g.Key, count = g.Count() }).OrderByDescending(ob => ob.count).Take(3).ToList();
+            int id = 1;
+            foreach (var item in countTopServices)
+            {
+                if (storeServiceDict.TryGetValue(item.serviceId, out var storeService))
+                {
+                    var ss = new StoreServiceResponse
+                    {
+                        Id = id,
+                        StoreName = storeService.Store.Name,
+                        ServiceName = storeService.Service.Name,
+                        NumberOfBooking = item.count
+                    };
+                    id++;
+                    topServices.Add(ss);
+                }
+            }
+
+            var response = new SMDashboardResponse
+            {
+                NumOfAll = bookings.Count(),
+                NumOfAccepted = bookings.Count(a => a.Status.Equals(BookingStatus.Accepted.ToString())),
+                NumOfCanceled = bookings.Count(c => c.Status.Equals(BookingStatus.Canceled.ToString())),
+                NumOfPending = bookings.Count(p => p.Status.Equals(BookingStatus.Pending.ToString())),
+                NumOfStores = 1,
+                NumOfReports = report.Count,
                 Revenues = revenues,
                 TopServices = topServices
             };
