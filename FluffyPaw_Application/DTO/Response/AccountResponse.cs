f70836c0609a6c5dc860887cad8d8bcd1a72faaf
Phạm Quốc Đạt <134580007+datpq-dev@users.noreply.github.com>
2024-12-08 21:53:07 +0700

    api for SM

--- FluffyPaw_Application/DTO/Response/AccountResponse.cs
+++ FluffyPaw_Application/DTO/Response/AccountResponse.cs
@@ -13,6 +13,7 @@ namespace FluffyPaw_Application.DTO.Response
         public long AccountId {  get; set; }
         public string Username { get; set; }
         public string Fullname { get; set; }
+        public string Avatar { get; set; }
         public string Email { get; set; }
         public string? Phone { get; set; }
         public DateTimeOffset? Dob { get; set; }
